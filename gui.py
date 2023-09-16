9ba8c2a9e5af35ae672715f79a0f3325db535ee3
Chinmay014 <chinmays117@gmail.com>
2023-09-16 20:01:05 +0200

    implement add and close button

--- gui.py
+++ gui.py
@@ -1,10 +1,25 @@
 import PySimpleGUI
+import functions
 
 # create a (filled) text box and input box
 label = PySimpleGUI.Text("Type in a to-do: ")
-input_box = PySimpleGUI.InputText(tooltip="write tasks here")
+input_box = PySimpleGUI.InputText(tooltip="write tasks here", key="todo")
 add_button = PySimpleGUI.Button("Add")
 
 # creates a window
-wdw = PySimpleGUI.Window('My To-do App',layout=[[label],[input_box,add_button]])
-wdw.read()
+wdw = PySimpleGUI.Window('My To-do App',
+                         layout=[[label],[input_box,add_button]], 
+                         font = ('Helvetica',20))
+while True:
+    tasks = functions.read_file()
+    print(tasks)
+    event,values = wdw.read()
+    print(event)
+    print(values)
+    match event:
+        case "Add":
+            new_task = values["todo"]+"\n"
+            tasks.append(new_task)
+            functions.write_to_file(tasks)
+        case PySimpleGUI.WIN_CLOSED:
+            break
